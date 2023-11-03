e0fb871ed521240f625ad3260bb768c87df04a8f
秦弘林 <1092750452@qq.com>
2023-11-03 16:02:55 +0800

    🐞 fix(git检测): 新增检测master

--- extension.js
+++ extension.js
@@ -1,5 +1,9 @@
 const vscode = require("vscode");
+const simpleGit = require("simple-git");
+
 const { checkGitFile } = require("./fs-service/operaFile.js");
+
+const workspace = vscode.workspace;
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -8,23 +12,40 @@ function activate(context) {
   const cleanGit = vscode.commands.registerCommand(
     "cleanLocalBranch",
     async () => {
-      const isDefaultBranch = await vscode.window.showInformationMessage(
-        "当前分支是否为默认分支?",
-        "是",
-        "否"
-      );
-      if (isDefaultBranch === "否") {
-        vscode.window.showErrorMessage("请先切换到默认分支,例如master");
-      } else {
-        const projectRoot =
-          vscode.workspace.workspaceFolders[0].uri.fsPath || "";
-        await checkGitFile(projectRoot, vscode);
-        // 在 checkGitFile 完成后执行 git pull 命令
-        const gitCommand = "git pull origin";
-        const terminal =
-          vscode.window.activeTerminal || vscode.window.createTerminal();
-        terminal.sendText(gitCommand);
-      }
+      const projectRoot = workspace.workspaceFolders[0].uri.fsPath || "";
+
+      // 使用 simple-git 或其他 Git 库来获取根目录下的分支信息
+      // @ts-ignore
+      const git = simpleGit(projectRoot);
+
+      git.branch(async (err, branchSummary) => {
+        if (err) {
+          console.error("Failed to get branch information:", err);
+          return;
+        }
+
+        const currentBranch = branchSummary.current;
+        if (currentBranch !== "master") {
+          vscode.window.showErrorMessage("请先切换到默认分支,例如master");
+          return;
+        } else {
+          const isDefaultBranch = await vscode.window.showInformationMessage(
+            "当前分支是否已经为默认分支?",
+            "是",
+            "否"
+          );
+          if (isDefaultBranch === "否") {
+            vscode.window.showErrorMessage("请先切换到默认分支,例如master");
+          } else {
+            await checkGitFile(projectRoot, vscode);
+            // 在 checkGitFile 完成后执行 git pull 命令
+            const gitCommand = "git pull origin";
+            const terminal =
+              vscode.window.activeTerminal || vscode.window.createTerminal();
+            terminal.sendText(gitCommand);
+          }
+        }
+      });
     }
   );
 
